964b32a55de1e3a15c67215287318eebaa39438d
Takeru SHIGEMORI <sgtakeru@gmail.com>
2015-09-18 19:28:14 +0900

    Add el-get

--- init.el
+++ init.el
@@ -1,3 +1,16 @@
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
+;; download el-get
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 (require 'pallet)
